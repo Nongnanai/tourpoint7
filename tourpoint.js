94bac5ec9b138ee5b49be85b187cc3bacceb4dab
Nongnanai <158020181+Nongnanai@users.noreply.github.com>
2024-05-14 20:08:48 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+var tourpoint=
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "khao Yai National park",
+        "type": "natural",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          101.49784821992864,
+          14.330157279739964
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "wat phra Kaew",
+        "level": 4,
+        "type": "cultural"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49280733667445,
+          13.751522411970484
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "iconsiam",
+        "level": 2,
+        "type": "spacial"
+      },
+      "geometry": {
+        "coordinates": [
+          100.51043650000179,
+          13.7266885708965
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Doi Inthanon",
+        "type": "natural",
+        "level": 4
+      },
+      "geometry": {
+        "coordinates": [
+          98.48695238721689,
+          18.590885092487085
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Doi Suthep",
+        "type": "natural",
+        "level": 3
+      },
+      "geometry": {
+        "coordinates": [
+          98.9202147288342,
+          18.804285281323246
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chiang Khan",
+        "type": "natural",
+        "level": 4
+      },
+      "geometry": {
+        "coordinates": [
+          101.6596890471182,
+          17.895272567723268
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "KI RI Wong",
+        "type": "natural",
+        "level": 4
+      },
+      "geometry": {
+        "coordinates": [
+          99.76002954232843,
+          8.440469102930805
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Chang Hai",
+        "type": "cultural",
+        "level": 3
+      },
+      "geometry": {
+        "coordinates": [
+          101.16896599999956,
+          6.668518400231363
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Gimyong Market",
+        "type": "spacial",
+        "level": 3
+      },
+      "geometry": {
+        "coordinates": [
+          100.47025800000131,
+          7.007783519143359
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ko Phithak",
+        "type": "natural",
+        "level": 3
+      },
+      "geometry": {
+        "coordinates": [
+          99.18842279500183,
+          10.05151566398986
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
